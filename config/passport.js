99f07a9af15d5870501605587ac8d34e3ac143e8
arfaoui47 <arfaoui47@gmail.com>
2016-07-19 20:23:06 +0200

    ADD: user signin

--- config/passport.js
+++ config/passport.js
@@ -17,6 +17,16 @@ passport.use('local.signup', new LocalStrategy({
     passwordField: 'password',
     passReqToCallback: true
 }, function (req, email, password, done){
+    req.checkBody('email', 'Invalid Email').notEmpty().isEmail();
+    req.checkBody('password', 'Invalid Password').notEmpty().isLength({min: 4});
+    var errors = req.validationErrors();
+    if (errors){
+        var messages = [];
+        errors.forEach(function (error) {
+            messages.push(error.msg);
+        });
+        return done(null, false, req.flash('error', messages));
+    }
     User.findOne({"email": email}, function(err, user) {
         if (err){
             return done(err);
@@ -33,5 +43,35 @@ passport.use('local.signup', new LocalStrategy({
             }
             return done(null, newUser);
         });
+    });
+}));
+
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+    
+}, function (req, email, password, done) {
+    req.checkBody('email', 'Invalid Email').notEmpty().isEmail();
+    req.checkBody('password', 'Invalid Password').notEmpty()
+    var errors = req.validationErrors();
+    if (errors){
+        var messages = [];
+        errors.forEach(function (error) {
+            messages.push(error.msg);
+        });
+        return done(null, false, req.flash('error', messages));
+    }
+    User.findOne({"email": email}, function(err, user) {
+        if (err){
+            return done(err);
+        }
+        if (!user){
+            return done(null, false, {message: 'no User found'});
+        }
+        if (!user.validPassword(password)){
+            return done(null, false, {message: 'wrong password'});
+        }
+        return done(null, user);
     });
 }));
--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,12 @@ var csrfProtection = csrf();
 router.use(csrfProtection);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 router.get('/user/signup', function (req, res, next) {
-  var messages = req.flash('error');
-  res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+    var messages = req.flash('error');
+    res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
 });
 
 router.post('/user/signup', passport.authenticate('local.signup', {
@@ -22,7 +22,17 @@ router.post('/user/signup', passport.authenticate('local.signup', {
 }));
 
 router.get('/user/profile', function (req, res, next) {
-  res.render('user/profile');
+    res.render('user/profile');
 });
 
+router.get('/user/signin', function (req, res, next) {
+    var messages = req.flash('error');
+    res.render('user/signin', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+});
+
+router.post('/user/signin', passport.authenticate('local.signin', {
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/signin',
+    failureFlash: true
+}));
 module.exports = router;
